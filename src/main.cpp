1d8786dd90a7f171f315aab60d45c686b18a2ba1
Vincent Savard <savard.vincent@gmail.com>
2016-06-02 19:59:47 -0400

    formatting main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -12,21 +12,21 @@
 #include <vector>
 
 std::unordered_set<std::string> get_allowed_characters() {
-	static const std::string allowed_characters_as_string = "abcdefghijklmnopqrstuvwxyz";
-	static std::unordered_set<std::string> allowed_characters;
+    static const std::string allowed_characters_as_string = "abcdefghijklmnopqrstuvwxyz";
+    static std::unordered_set<std::string> allowed_characters;
 
-	if (allowed_characters.empty()) {
-		for (const char character : allowed_characters_as_string) {
-			allowed_characters.emplace(std::string(1, character));
-		}
-	}
+    if (allowed_characters.empty()) {
+        for (const char character : allowed_characters_as_string) {
+            allowed_characters.emplace(std::string(1, character));
+        }
+    }
 
-	return allowed_characters;
+    return allowed_characters;
 }
 
 int main() {
     using namespace wordfinder::grid;
-	using namespace wordfinder::dictionary;
+    using namespace wordfinder::dictionary;
 
     GridSize grid_size(2, 2);
     std::unique_ptr<AdjacentPositionsFinder> adjacent_positions_finder = std::make_unique<DiagonalAdjacentPositionsFinder>(grid_size);
@@ -43,19 +43,19 @@ int main() {
         std::cout << combination << std::endl;
     }
 
-	std::unique_ptr<Dictionary> dictionary = std::make_unique<TrieDictionaryContainer>(get_allowed_characters());
+    std::unique_ptr<Dictionary> dictionary = std::make_unique<TrieDictionaryContainer>(get_allowed_characters());
 
-	dictionary->add_word("cat");
-	dictionary->add_word("cat");
-	dictionary->add_word("car");
-	dictionary->add_word("corn");
-	dictionary->add_word("alpha");
-	dictionary->add_word("aujourd'hui");
-	dictionary->add_word("sur-le-champ");
+    dictionary->add_word("cat");
+    dictionary->add_word("cat");
+    dictionary->add_word("car");
+    dictionary->add_word("corn");
+    dictionary->add_word("alpha");
+    dictionary->add_word("aujourd'hui");
+    dictionary->add_word("sur-le-champ");
 
-	std::cout << dictionary->word_exists("abc") << std::endl;
-	std::cout << dictionary->radix_exists("ab") << std::endl;
-	std::cout << dictionary->word_exists("ab") << std::endl;
+    std::cout << dictionary->word_exists("abc") << std::endl;
+    std::cout << dictionary->radix_exists("ab") << std::endl;
+    std::cout << dictionary->word_exists("ab") << std::endl;
 
     return 0;
 }
