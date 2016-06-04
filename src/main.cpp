8d9721b00f2b2cd9da17f50a69ff3641378e3a7b
Vincent Savard <savard.vincent@gmail.com>
2016-06-04 16:47:00 -0400

    loading a grid from a text file

--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,8 @@
 #include <unordered_set>
 #include <vector>
 
-constexpr const size_t ROW_COUNT = 2;
-constexpr const size_t COLUMN_COUNT = 2;
+constexpr const size_t ROW_COUNT = 4;
+constexpr const size_t COLUMN_COUNT = 4;
 constexpr const size_t MINIMUM_WORD_LENGTH = 3;
 constexpr const char* ALLOWED_CHARACTERS = "abcdefghijklmnopqrstuvwxyz";
 
@@ -42,11 +42,29 @@ void fill_dictionary(wordfinder::dictionary::Dictionary* dictionary, const std::
     }
 }
 
-void fill_grid(wordfinder::grid::Grid* grid) {
-    grid->fill_cell(wordfinder::grid::Position(0, 0), std::vector<std::string>{ "c" });
-    grid->fill_cell(wordfinder::grid::Position(0, 1), std::vector<std::string>{ "a" });
-    grid->fill_cell(wordfinder::grid::Position(1, 0), std::vector<std::string>{ "r" });
-    grid->fill_cell(wordfinder::grid::Position(1, 1), std::vector<std::string>{ "t" });
+void fill_grid(wordfinder::grid::Grid* grid, const std::string& filepath) {
+    using namespace wordfinder::grid;
+
+    std::ifstream file_stream(filepath);
+    std::stringstream line_stream;
+    std::string current_line;
+    std::string current_cell;
+    size_t index = 0;
+    size_t i;
+    size_t j;
+
+    while (std::getline(file_stream, current_line)) {
+        line_stream = std::stringstream(current_line);
+
+        while (std::getline(line_stream, current_cell, ' ')) {
+            i = index / ROW_COUNT;
+            j = index % COLUMN_COUNT;
+
+            grid->fill_cell(Position(i, j), std::vector<std::string>{ current_cell });
+
+            index++;
+        }
+    }
 }
 
 std::unique_ptr<wordfinder::dictionary::Dictionary> build_dictionary(const std::string& filepath) {
@@ -58,14 +76,17 @@ std::unique_ptr<wordfinder::dictionary::Dictionary> build_dictionary(const std::
     return std::move(dictionary);
 }
 
-std::unique_ptr<wordfinder::grid::Grid> build_grid(std::unique_ptr<wordfinder::dictionary::Dictionary> dictionary) {
+std::unique_ptr<wordfinder::grid::Grid> build_grid(
+    std::unique_ptr<wordfinder::dictionary::Dictionary> dictionary,
+    const std::string& filepath
+) {
     using namespace wordfinder::grid;
 
     GridSize grid_size(ROW_COUNT, COLUMN_COUNT);
     std::unique_ptr<AdjacentPositionsFinder> adjacent_positions_finder = std::make_unique<DiagonalAdjacentPositionsFinder>(grid_size);
     std::unique_ptr<Grid> grid = std::make_unique<Grid>(grid_size, std::move(adjacent_positions_finder), std::move(dictionary), MINIMUM_WORD_LENGTH);
 
-    fill_grid(grid.get());
+    fill_grid(grid.get(), filepath);
 
     return std::move(grid);
 }
@@ -74,15 +95,18 @@ int main(int argc, char* argv[]) {
     using namespace wordfinder::grid;
     using namespace wordfinder::dictionary;
 
-    if (argc < 2) {
-        std::cout << "usage: wordfinder.exe DICTIONARY" << std::endl;
-        std::cout << "A DICTIONARY is a path to the file containing the words." << std::endl;
+    if (argc < 3) {
+        std::cout << "usage: wordfinder.exe DICTIONARY GRID" << std::endl;
+        std::cout << "DICTIONARY is the path to the file containing the words." << std::endl;
+        std::cout << "GRID is the path to the file containing the grid." << std::endl;
+
         return 1;
     }
 
     std::string dictionary_filepath(argv[1]);
+    std::string grid_filepath(argv[2]);
     std::unique_ptr<Dictionary> dictionary = std::move(build_dictionary(dictionary_filepath));
-    std::unique_ptr<Grid> grid = std::move(build_grid(std::move(dictionary)));
+    std::unique_ptr<Grid> grid = std::move(build_grid(std::move(dictionary), grid_filepath));
 
     std::vector<std::string> words_found(grid->find_words());
 
